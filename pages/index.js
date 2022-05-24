a45993912cbc92383251347603068c1b23f0b0fe
valeriyshashkin <velianjanjan@gmail.com>
2022-05-25 00:37:29 +0500

    add week navigation

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import Autosuggest from "react-autosuggest";
 import Head from "next/head";
-
-const startDate = "23.05.2022";
-const endDate = "29.05.2022";
+import startOfWeek from "date-fns/startOfWeek";
+import endOfWeek from "date-fns/endOfWeek";
+import format from "date-fns/format";
 
 function getSuggestionValue(s) {
   return s;
@@ -13,11 +13,30 @@ function renderSuggestion(s) {
   return s;
 }
 
+function getStartDay(delta) {
+  return format(
+    startOfWeek(Date.now() + 7 * 24 * 60 * 60 * 1000 * delta, {
+      weekStartsOn: 1,
+    }),
+    "dd.MM.yyyy"
+  );
+}
+
+function getEndDay(delta) {
+  return format(
+    endOfWeek(Date.now() + 7 * 24 * 60 * 60 * 1000 * delta, {
+      weekStartsOn: 1,
+    }),
+    "dd.MM.yyyy"
+  );
+}
+
 export default function Home() {
   const [group, setGroup] = useState("");
   const [schedule, setSchedule] = useState([]);
   const [suggestions, setSuggestions] = useState([]);
   const [allSuggestions, setAllSuggestions] = useState([]);
+  const [weekDelta, setWeekDelta] = useState(0);
 
   useEffect(() => {
     setGroup(localStorage.getItem("group") || "");
@@ -30,14 +49,53 @@ export default function Home() {
   }, []);
 
   function showSchedule() {
+    setWeekDelta(0);
     fetch("/api/schedule", {
       method: "POST",
-      body: JSON.stringify({ startDate, endDate, group }),
+      body: JSON.stringify({
+        startDate: getStartDay(0),
+        endDate: getEndDay(0),
+        group,
+      }),
     })
       .then((res) => res.json())
       .then((res) => {
         localStorage.setItem("group", group);
         localStorage.setItem("schedule", JSON.stringify(res));
+        setSchedule(res);
+      });
+  }
+
+  function past() {
+    setWeekDelta(weekDelta - 1);
+
+    fetch("/api/schedule", {
+      method: "POST",
+      body: JSON.stringify({
+        startDate: getStartDay(weekDelta - 1),
+        endDate: getEndDay(weekDelta - 1),
+        group,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setSchedule(res);
+      });
+  }
+
+  function future() {
+    setWeekDelta(weekDelta + 1);
+
+    fetch("/api/schedule", {
+      method: "POST",
+      body: JSON.stringify({
+        startDate: getStartDay(weekDelta + 1),
+        endDate: getEndDay(weekDelta + 1),
+        group,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
         setSchedule(res);
       });
   }
@@ -90,8 +148,12 @@ export default function Home() {
       <div className="m-4 mt-0 w-full">
         {schedule.length > 0 && (
           <div className="btn-group grid grid-cols-2 mt-4 w-full">
-            <button className="btn btn-outline">Прошлое</button>
-            <button className="btn btn-outline">Будущее</button>
+            <button className="btn btn-outline" onClick={past}>
+              Прошлое
+            </button>
+            <button className="btn btn-outline" onClick={future}>
+              Будущее
+            </button>
           </div>
         )}
         {schedule.map(({ date, pairs, weekDay }, id) => (
