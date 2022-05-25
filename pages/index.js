30e39b27f21cecf1d689df6805c49c816d7ab5b3
valeriyshashkin <velianjanjan@gmail.com>
2022-05-25 22:19:04 +0500

    replace arrows with infinite scroll and button show previous days, remove show schedule button for the sake of selecting from autosuggest

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 import Autosuggest from "react-autosuggest";
 import Head from "next/head";
-import startOfWeek from "date-fns/startOfWeek";
-import endOfWeek from "date-fns/endOfWeek";
-import format from "date-fns/format";
-import {
-  StatusOfflineIcon,
-  ArrowNarrowLeftIcon,
-  ArrowNarrowRightIcon,
-} from "@heroicons/react/outline";
+import { StatusOfflineIcon } from "@heroicons/react/outline";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { format, add, sub } from "date-fns";
 
 function getSuggestionValue(s) {
   return s;
@@ -41,26 +36,18 @@ export default function Home() {
   const [schedule, setSchedule] = useState([]);
   const [suggestions, setSuggestions] = useState([]);
   const [allSuggestions, setAllSuggestions] = useState([]);
-  const [weekDelta, setWeekDelta] = useState(0);
   const [offline, setOffline] = useState(false);
   const [loading, setLoading] = useState(false);
   const [refetch, setRefetch] = useState(true);
+  const startDateRef = useRef();
+  const endDateRef = useRef();
 
-  const todayRef = useCallback((node) => {
-    if (node !== null) {
-      node.scrollIntoView();
-    }
-  }, []);
-
-  function showSchedule(e) {
-    e?.preventDefault();
-    setWeekDelta(0);
-    setLoading(true);
+  function showSchedule() {
     fetch("/api/schedule", {
       method: "POST",
       body: JSON.stringify({
-        startDate: getStartDay(0),
-        endDate: getEndDay(0),
+        startDate: format(startDateRef.current, "dd.MM.yyyy"),
+        endDate: format(endDateRef.current, "dd.MM.yyyy"),
         group,
       }),
     })
@@ -77,6 +64,9 @@ export default function Home() {
   showScheduleFunc.current = showSchedule;
 
   useEffect(() => {
+    startDateRef.current = Date.now();
+    endDateRef.current = add(Date.now(), { days: 7 });
+
     function onOnline() {
       setOffline(false);
       showScheduleFunc.current();
@@ -110,44 +100,6 @@ export default function Home() {
     setRefetch(false);
   }, [group, refetch]);
 
-  function past() {
-    setLoading(true);
-    setWeekDelta(weekDelta - 1);
-
-    fetch("/api/schedule", {
-      method: "POST",
-      body: JSON.stringify({
-        startDate: getStartDay(weekDelta - 1),
-        endDate: getEndDay(weekDelta - 1),
-        group,
-      }),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setSchedule(res);
-        setLoading(false);
-      });
-  }
-
-  function future() {
-    setLoading(true);
-    setWeekDelta(weekDelta + 1);
-
-    fetch("/api/schedule", {
-      method: "POST",
-      body: JSON.stringify({
-        startDate: getStartDay(weekDelta + 1),
-        endDate: getEndDay(weekDelta + 1),
-        group,
-      }),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setSchedule(res);
-        setLoading(false);
-      });
-  }
-
   function handleChange(e, { newValue }) {
     setGroup(newValue);
   }
@@ -174,6 +126,21 @@ export default function Home() {
     onChange: handleChange,
   };
 
+  function next() {
+    if (offline) {
+      return;
+    }
+
+    endDateRef.current = add(endDateRef.current, { days: 7 });
+    showSchedule();
+  }
+
+  function prev() {
+    setLoading(true);
+    startDateRef.current = sub(startDateRef.current, { days: 7 });
+    showSchedule();
+  }
+
   return (
     <>
       {offline && (
@@ -188,124 +155,130 @@ export default function Home() {
           <meta name="theme-color" content="#2a303c" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
-        <form onSubmit={showSchedule}>
-          <Autosuggest
-            suggestions={suggestions}
-            onSuggestionsFetchRequested={fetchSuggestions}
-            onSuggestionsClearRequested={clearSuggestions}
-            renderSuggestion={renderSuggestion}
-            inputProps={inputProps}
-            getSuggestionValue={getSuggestionValue}
-          />
-
-          <button
-            className="btn btn-primary w-full mx-4 mt-0"
-            type="submit"
-            disabled={offline}
-          >
-            Показать расписание
-          </button>
-        </form>
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={fetchSuggestions}
+          onSuggestionsClearRequested={clearSuggestions}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+          getSuggestionValue={getSuggestionValue}
+          highlightFirstSuggestion
+          onSuggestionSelected={showSchedule}
+        />
+        <button
+          className={`w-full mx-4 mt-4 btn btn-outline !no-underline${
+            loading && " loading"
+          }`}
+          onClick={prev}
+          disabled={offline}
+        >
+          Показать предыдущие дни
+        </button>
         <div className="m-4 mt-0 w-full">
-          {schedule.length > 0 && (
-            <div className="btn-group grid grid-cols-2 mt-4 w-full">
-              <button
-                className="btn btn-outline"
-                onClick={past}
-                disabled={offline}
-              >
-                <ArrowNarrowLeftIcon className="h-6 w-6" />
-              </button>
-              <button
-                className="btn btn-outline"
-                onClick={future}
-                disabled={offline}
-              >
-                <ArrowNarrowRightIcon className="h-6 w-6" />
-              </button>
-            </div>
-          )}
-          {loading ? (
-            <>
-              <div className="mt-4 p-4 bg-gray-700 h-[24px] rounded-lg"></div>
-              <div className="mt-4 bg-gray-700 h-[316px] rounded-lg"></div>
-              <div className="mt-4 p-4 bg-gray-700 h-[24px] rounded-lg"></div>
-              <div className="mt-4 bg-gray-700 h-[316px] rounded-lg"></div>
-              <div className="mt-4 p-4 bg-gray-700 h-[24px] rounded-lg"></div>
-              <div className="mt-4 bg-gray-700 h-[316px] rounded-lg"></div>
-            </>
-          ) : (
-            schedule.map(({ date, pairs, isCurrentDate, weekDay }, id) => (
-              <div key={id}>
-                {isCurrentDate !== 0 ? (
-                  <h3
-                    ref={todayRef}
-                    className="w-full h-[56px] flex justify-center items-center font-bold"
-                  >
-                    {date} - {weekDay}
-                    <span className="badge badge-primary ml-2">Сегодня</span>
-                  </h3>
-                ) : (
+          <InfiniteScroll
+            dataLength={schedule.length}
+            next={next}
+            hasMore
+            loader={
+              !offline && (
+                <>
+                  <div className="mt-4 p-4 bg-gray-700 h-[24px] rounded-lg"></div>
+                  <div className="mt-4 bg-gray-700 h-[316px] rounded-lg"></div>
+                  <div className="mt-4 p-4 bg-gray-700 h-[24px] rounded-lg"></div>
+                  <div className="mt-4 bg-gray-700 h-[316px] rounded-lg"></div>
+                  <div className="mt-4 p-4 bg-gray-700 h-[24px] rounded-lg"></div>
+                  <div className="mt-4 bg-gray-700 h-[316px] rounded-lg"></div>
+                </>
+              )
+            }
+          >
+            {loading ? (
+              <>
+                <div className="mt-4 p-4 bg-gray-700 h-[24px] rounded-lg"></div>
+                <div className="mt-4 bg-gray-700 h-[316px] rounded-lg"></div>
+                <div className="mt-4 p-4 bg-gray-700 h-[24px] rounded-lg"></div>
+                <div className="mt-4 bg-gray-700 h-[316px] rounded-lg"></div>
+                <div className="mt-4 p-4 bg-gray-700 h-[24px] rounded-lg"></div>
+                <div className="mt-4 bg-gray-700 h-[316px] rounded-lg"></div>
+              </>
+            ) : (
+              schedule.map(({ date, pairs, isCurrentDate, weekDay }, id) => (
+                <div key={id}>
                   <h3 className="w-full h-[56px] flex justify-center items-center font-bold">
                     {date} - {weekDay}
+                    {isCurrentDate !== 0 && (
+                      <span className="badge badge-primary ml-2">Сегодня</span>
+                    )}
                   </h3>
-                )}
-                <div className="overflow-x-auto">
-                  <table className="table table-compact table-zebra w-full">
-                    <thead>
-                      <tr>
-                        <th></th>
-                        <th>Время</th>
-                        <th>Предмет</th>
-                        <th>Преподаватель</th>
-                        <th>Аудитория</th>
-                        <th>Группа</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {pairs.map(
-                        ({ N, time, isCurrentPair, schedulePairs }, id) =>
-                          time !== "-" && (
-                            <tr key={id}>
-                              <th
-                                className={`${isCurrentPair && "!bg-primary"}`}
-                              >
-                                {N}
-                              </th>
-                              <td
-                                className={`${isCurrentPair && "!bg-primary"}`}
-                              >
-                                {time}
-                              </td>
-                              <td
-                                className={`${isCurrentPair && "!bg-primary"}`}
-                              >
-                                {schedulePairs[0]?.subject}
-                              </td>
-                              <td
-                                className={`${isCurrentPair && "!bg-primary"}`}
-                              >
-                                {schedulePairs[0]?.teacher}
-                              </td>
-                              <td
-                                className={`${isCurrentPair && "!bg-primary"}`}
-                              >
-                                {schedulePairs[0]?.aud}
-                              </td>
-                              <td
-                                className={`${isCurrentPair && "!bg-primary"}`}
-                              >
-                                {schedulePairs[0]?.group}
-                              </td>
-                            </tr>
-                          )
-                      )}
-                    </tbody>
-                  </table>
+                  <div className="overflow-x-auto">
+                    <table className="table table-compact table-zebra w-full">
+                      <thead>
+                        <tr>
+                          <th></th>
+                          <th>Время</th>
+                          <th>Предмет</th>
+                          <th>Преподаватель</th>
+                          <th>Аудитория</th>
+                          <th>Группа</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {pairs.map(
+                          ({ N, time, isCurrentPair, schedulePairs }, id) =>
+                            time !== "-" && (
+                              <tr key={id}>
+                                <th
+                                  className={`${
+                                    isCurrentPair && "!bg-primary"
+                                  }`}
+                                >
+                                  {N}
+                                </th>
+                                <td
+                                  className={`${
+                                    isCurrentPair && "!bg-primary"
+                                  }`}
+                                >
+                                  {time}
+                                </td>
+                                <td
+                                  className={`${
+                                    isCurrentPair && "!bg-primary"
+                                  }`}
+                                >
+                                  {schedulePairs[0]?.subject}
+                                </td>
+                                <td
+                                  className={`${
+                                    isCurrentPair && "!bg-primary"
+                                  }`}
+                                >
+                                  {schedulePairs[0]?.teacher}
+                                </td>
+                                <td
+                                  className={`${
+                                    isCurrentPair && "!bg-primary"
+                                  }`}
+                                >
+                                  {schedulePairs[0]?.aud}
+                                </td>
+                                <td
+                                  className={`${
+                                    isCurrentPair && "!bg-primary"
+                                  }`}
+                                >
+                                  {schedulePairs[0]?.group}
+                                </td>
+                              </tr>
+                            )
+                        )}
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
-              </div>
-            ))
-          )}
+              ))
+            )}
+          </InfiniteScroll>
           <a
             href="https://www.flaticon.com/free-icons/calendar"
             title="calendar icons"
