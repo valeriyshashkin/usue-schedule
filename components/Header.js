b82ff4b8a13d2e387c3f39d89624abb7970cfb57
valeriyshashkin <velianjanjan@gmail.com>
2022-10-07 16:40:23 +0500

    поменял иконку

--- components/Header.js
+++ components/Header.js
@@ -16,7 +16,19 @@ export default function Header({ homepage, onSearchClick, loading }) {
           </a>
         </Link>
       )}
-      <h1 className="font-bold text-2xl">ush</h1>
+
+      <svg
+        height="20"
+        viewBox="0 0 374 110"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M0 0H110L55 110V110L0 0Z" fill="#FC62FF" />
+        <path d="M132 27.5L187 0V55L132 27.5Z" fill="#66BFFF" />
+        <path d="M242 82.5L187 110L187 55L242 82.5Z" fill="#66BFFF" />
+        <path d="M264 0L319 55L374 110H264V0Z" fill="#A975FF" />
+      </svg>
+
       <button onClick={onSearchClick}>
         {loading ? (
           <div className="w-6"></div>
--- pages/index.js
+++ pages/index.js
@@ -11,16 +11,18 @@ export default function Home() {
       </Head>
       <Page homepage>
         <div className="h-[calc(100vh-120px)] flex flex-col justify-center">
-          <div className="h-[150px] relative">
-            <Image
-              src="/cat.gif"
-              layout="fill"
-              objectFit="contain"
-              priority
-              alt=""
-            />
-          </div>
-          <div className="text-center">
+          <svg
+            height="50"
+            viewBox="0 0 374 110"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0H110L55 110V110L0 0Z" fill="#FC62FF" />
+            <path d="M132 27.5L187 0V55L132 27.5Z" fill="#66BFFF" />
+            <path d="M242 82.5L187 110L187 55L242 82.5Z" fill="#66BFFF" />
+            <path d="M264 0L319 55L374 110H264V0Z" fill="#A975FF" />
+          </svg>
+          <div className="text-center mt-10 text-lg">
             Начните искать расписание группы или преподавателя
           </div>
         </div>
